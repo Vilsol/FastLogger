1e861fcc2671a62cb50b423b26d429dea5f01018
Vilsol <vilsol2000@gmail.com>
2017-11-14 01:24:18 +0200

    Incorrect month length

--- static/js.js
+++ static/js.js
@@ -95,7 +95,7 @@ $(document).ready(function () {
                     }
                 }
 
-                if(diff < 18144000){
+                if(diff < 2592000){
                     oneMonthSum += e.speed;
                     oneMonthCount++;
 
